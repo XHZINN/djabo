d7e106e266d6bf5d9d86b767dede6328611d5271
XHZINN <gabrielbr1712Hg@gmail.com>
2025-09-21 13:19:53 -0300

    imagens fix

--- server.js
+++ server.js
@@ -64,8 +64,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-
+app.use('/uploads', express.static('uploads'));
 
 
 // ============================
